cac920ccb1be15d83f7a52d40aeca93ae106b729
Jeffrey Marcilliat <jeffmarcilliat@mac.com>
2024-07-10 11:38:01 -0700

    Initial Commit

---
+++ simpleclock/simpleclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleclockApp.swift
+//  simpleclock
+//
+//  Created by Jeffrey Marcilliat on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct simpleclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simpleclockTests/simpleclockTests.swift
@@ -0,0 +1,17 @@
+//
+//  simpleclockTests.swift
+//  simpleclockTests
+//
+//  Created by Jeffrey Marcilliat on 7/10/24.
+//
+
+import Testing
+@testable import simpleclock
+
+struct simpleclockTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
